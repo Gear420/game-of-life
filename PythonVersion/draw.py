0bd849a5e23fdb3967deb71f86e2c725751787ef
XingyuZhang <study_zxy@163.com>
2018-06-09 15:49:18 +0800

    Add files via upload

--- PythonVersion/draw.py
+++ PythonVersion/draw.py
@@ -4,18 +4,35 @@ import time
 
 
 class cellSet(Frame):
-    def __init__(self,g,master=None):
+    def __init__(self,cellGraph,master):
         self.root = master  # 定义内部变量root
-        super().__init__(master)
+        Frame.__init__(self, master)
         self.root.geometry('%dx%d' % (400,400))  # 设置窗口大小
+        self.cellGraph = cellGraph
+        self.updateUI()
+        print("now quit!")
+        #self.destroy()
+        # self.closewindow()
+
+    def updateUI(self):
+        g = self.cellGraph.cell_g()
         for i in range(20):
             for j in range(20):
                 if g[i][j] == 1:
-                    self.create(i,j)
-        #self.dele()
+                    self.create(i, j)
+                if g[i][j] == 0:
+                    self.delete(i,j)
+        self.cellGraph.chanege_status()
+        self.after(1000, self.updateUI)
+
 
     def create(self,x,y):
         self.cell = Frame(self.root, width=20, height=20, bg="red")
         self.cell.place(x=x*20,y = y*20,width = 20,height =20 )
-    def dele(self):
-        self.quit()
+
+    def delete(self, x, y):
+        self.cell = Frame(self.root, width=20, height=20, bg="white")
+        self.cell.place(x=x * 20, y=y * 20, width=20, height=20)
+    # def closewindow(self):
+    #     # time.sleep(10)
+    #     self.root.quit()
--- PythonVersion/main.py
+++ PythonVersion/main.py
@@ -3,13 +3,12 @@ from draw import cellSet
 from tkinter import *
 import time
 
+#
+# def autoclose():
+#
+
 cellGraph = Graph(20)
-for t in range(100):
-    root = Tk()
-    root.title("draw")
-    g = cellGraph.cell_g()
-    print(g)
-    app = cellSet(g,root)
-    root.mainloop()
-    cellGraph.chanege_status()
-    root.quit()
+root = Tk()
+root.title("draw")
+cellSet(cellGraph, root)
+root.mainloop()
